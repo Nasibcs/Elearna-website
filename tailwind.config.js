16662661479c175cf9848dbe235ec9504845388d
nasibburhan37@gmail.com <nasibburhan37@email.com>
2025-08-09 14:47:24 +0430

    Update Home.jsx and other recent changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        bannerImg: "url('./src/Components/Home/images/homeImage.jpg')",
+       bannerImg: "url('/images/homeImage.jpg')",
         homeOnebanner: "url('./src/components/Home/images/homeonebg.avif')",
         homeonebanner: "url('./src/components/HomeOne/image/homeoneImage.webp')",
         blackOverlay: "linear-gradient(to bottom, rgba(0,0,0,0) 0%, rgba(0,0,0,1.5) 100%)",
